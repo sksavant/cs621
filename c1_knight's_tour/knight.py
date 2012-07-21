1b3ceeceac4b58f489e9c618ad2fee0041cc8d57
Savant Krishna <savant.2020@gmail.com>
2012-07-21 19:37:47 +0530

    Added comments, plan to knight's tour

---
+++ c1_knight's_tour/knight.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2012 Savant Krishna
+# 
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by Free Software 
+# Foundation; either version 3 or (at your option) higher
+# 
+# This program tries to solve the knight's tour problem
+# 
+# Problem Statement: 
+# In a chessboard of size 8X8 , given a position of a knight, find a tour 
+# which will go to each of the squares exactly once.
+# 
+# Implementation:
+# There are known solutions for the knight's tour using 
+# *Divide and Conquer algorithms
+# *Neural Networks :
+# *Heurestic methods : Warnsdorff's rule
+# 
+# This program implements the Warnsdorff's rule using tie-breaking techniques
+# devised by Pohl?/ (new heurestic?)
